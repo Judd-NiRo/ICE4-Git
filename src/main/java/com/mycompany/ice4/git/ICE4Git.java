0891e3e54ce8b23920eb971c2ec5505f82726612
judds <judds@10.48.171.181>
2026-01-28 15:40:05 -0500

    Initial Commit

---
+++ src/main/java/com/mycompany/ice4/git/ICE4Git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice4.git;
+
+/**
+ *
+ * @author judds
+ */
+public class ICE4Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
